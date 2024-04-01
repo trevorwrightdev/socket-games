3a822a7ea875c901d103f8b45c8fd75569191c89
trevdev <trevorwrightdev@gmail.com>
2024-04-01 11:46:02 -0700

    Made font bold when hovering.

--- frontend/app/components/CoolLink.tsx
+++ frontend/app/components/CoolLink.tsx
@@ -10,14 +10,14 @@ type CoolLinkProps = {
 
 const CoolLink:React.FC<CoolLinkProps> = ({ children, href }) => {
     
-    const [hover, setHover] = useState<boolean>(false)
+    const [isHovering, setIsHovering] = useState<boolean>(false)
 
     return (
         <Link href={href}>
-            <div className='flex flex-col items-center' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            <div className={`${isHovering ? 'font-bold' : ''} flex flex-col items-center`} onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
                 {children}
                 <motion.div
-                    animate={{ width: hover ? '100%' : 0 }}
+                    animate={{ width: isHovering ? '100%' : 0 }}
                     transition={{ duration: 0.25 }}
                     style={{ background: '#434343', height: '2px' }}
                 />
