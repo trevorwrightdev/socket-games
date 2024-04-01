d83b7f0c833ab75009219c29ea8d06372b8e47e3
trevdev <trevorwrightdev@gmail.com>
2024-04-01 11:48:53 -0700

    Added a small touch the the main page.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,18 @@
 import CoolLink from 'components/CoolLink'
 
 const links = [
-    { title: 'Secret Hitler', href: '/secrethitler' }
+    { title: 'Secret Hitler', href: '/secrethitler' },
 ]
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center pt-24">
-        <h1 className="text-3xl">trevdev.fun</h1>
-        {links.map((link) => {
-            return <CoolLink key={link.title} href={link.href}>{link.title}</CoolLink>
-        })}
+        <h1 className="text-3xl mb-4">trevdev.fun</h1>
+        <div>
+            {links.map((link) => {
+                return <CoolLink key={link.title} href={link.href}>{link.title}</CoolLink>
+            })}
+        </div>
     </main>
   )
 }
