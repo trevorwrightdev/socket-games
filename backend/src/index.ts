4c0164370c848e98facd94b98104a97c0c582473
trevdev <trevorwrightdev@gmail.com>
2024-04-01 16:28:11 -0700

    set up to work with nodemon

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,5 +26,5 @@ io.on('connection', (socket) => {
 // Start the server
 const PORT = process.env.PORT || 3001
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}.`)
 })
