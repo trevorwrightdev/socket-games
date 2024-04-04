48a3c4b2d0ad98520e1c6ec9f72e94a49c19713e
trevdev <trevorwrightdev@gmail.com>
2024-04-04 15:01:03 -0700

    set up play page

--- frontend/app/secrethitler/play/page.tsx
+++ frontend/app/secrethitler/play/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 import { useGlobalState } from '@/app/components/GlobalContextProvider'
+import RainbowText from '@/app/components/RainbowText'
 
 export default function Play() {
 
     const [globalState, updateGlobalState] = useGlobalState()
 
     return (
-        <div>
-            Secret Hitler {globalState.roomCode}
+        <div className='flex flex-col items-center'>
+            <div className='bg-white w-full h-12 grid place-items-center'>
+                <RainbowText className='text-xl font-bold'>Secret Hitler</RainbowText>
+            </div>
         </div>
     )    
 }
