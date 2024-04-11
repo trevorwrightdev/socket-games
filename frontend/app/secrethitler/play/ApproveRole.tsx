5bf0b10414b40c4f6b4c852be9ba839b0a8c6fad
trevdev <trevorwrightdev@gmail.com>
2024-04-11 15:59:45 -0700

    added emojis to the role screen

--- frontend/app/secrethitler/play/ApproveRole.tsx
+++ frontend/app/secrethitler/play/ApproveRole.tsx
@@ -9,16 +9,16 @@ const ApproveRole:React.FC<ApproveRoleProps> = ({ roleData }) => {
     return (
         <div className='flex flex-col items-center'>
             {roleData.role === 'hitler' && (
-                <p>You are HITLER.</p>
+                <p>😈 You are HITLER. 😈</p>
             )}
             {(!roleData.otherFascists || roleData.otherFascists.length === 0) && roleData.role === 'hitler' && (
                 <p className='text-center'>You do not know who your fellow fascists are, but they know that you are Hitler.</p>
             )}
             {roleData.role === 'fascist' && (
-                <p>You are a FASCIST.</p>
+                <p>🔪 You are a FASCIST. 🔪</p>
             )}
             {roleData.role === 'liberal' && (
-                <p>You are a LIBERAL.</p>
+                <p>😊 You are a LIBERAL. 😊</p>
             )}
             {roleData.hitler && (
                 <p>{roleData.hitler} is HITLER.</p>
